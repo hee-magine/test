f1efa9c641a0936bf7a2536c00a1123e077d1d16
hee-magine <myheesan@gmail.com>
2023-04-28 22:34:40 +0900

    test2

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
+
+// import { API_KEY } from "./apikey.js";
+// import { PROMPT } from "./prompt.js";
+const configuration = new Configuration({
+    organization: "org-xctP8O1XB6NLY9K7RK9xnKiM",
+    apiKey: `${API_KEY}`
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", function (req, res) {
     res.send("Hello World");
